1ad64dea7173a50aa32ef3255279d48a4c3007b5
Kansha Guna <47423150+kanshaguna@users.noreply.github.com>
2025-01-17 13:55:55 +0800

    Update

--- server/db.js
+++ server/db.js
@@ -1,14 +1,15 @@
 const { Pool } = require("pg");
+// require('dotenv').config();
 
 const pool = new Pool({
   user: "example",
-  password: "your_password_here",
+  password: "your_password",
   host: "localhost",
   port: "5432",
   database: "task_manager_db"
   
 });
 
-console.log("Connected to DB");
+console.log("Connected to DB2");
 
 module.exports = pool;
